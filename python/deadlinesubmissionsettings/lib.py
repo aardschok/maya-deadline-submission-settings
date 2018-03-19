564df18211e5fd5abec2b3e8658095d98d7ba3a0
wijnand <wikoreman@gmail.com>
2018-03-19 11:34:18 +0100

    Using api.Session, added get_work_directory

--- python/deadlinesubmissionsettings/lib.py
+++ python/deadlinesubmissionsettings/lib.py
@@ -6,11 +6,31 @@ Dependencies:
 
 import logging
 
-import avalon
+import os
+
+from avalon import io, api, pipeline
 from avalon.vendor import requests
 
 
 log = logging.getLogger("DSS - Lib")
+
+
+def get_work_directory():
+    """Get the work directory to Fusion through the template"""
+
+    session = api.Session.copy()
+    project = io.find_one({"type": "project",
+                           "name": api.Session["AVALON_PROJECT"]})
+    assert project, "This is a bug"
+    template = project["config"]["template"]["work"]
+
+    session["AVALON_APP"] = "fusion"
+    work_directory = pipeline._format_work_template(template, session)
+    if not os.path.isdir(work_directory):
+        print("Fusion app folder does not exist:\n%s" % work_directory)
+        work_directory = pipeline._format_work_template(template, api.Session)
+
+    return work_directory
 
 
 def query(argument):
@@ -29,21 +49,21 @@ def query(argument):
 
 def get_machine_list(debug=None):
     """Fetch the machine list (slaves) from Deadline"""
-    AVALON_SESSION = debug or avalon.Session["AVALON_DEADLINE"]
+    AVALON_SESSION = debug or api.Session["AVALON_DEADLINE"]
     argument = "{}/api/slaves?NamesOnly=true".format(AVALON_SESSION)
     return query(argument=argument)
 
 
 def get_pool_list(debug=None):
     """Get all pools from Deadline"""
-    AVALON_SESSION = debug or avalon.Session["AVALON_DEADLINE"]
+    AVALON_SESSION = debug or api.Session["AVALON_DEADLINE"]
     argument = "{}/api/pools?NamesOnly=true".format(AVALON_SESSION)
     return query(argument)
 
 
 def get_group_list(debug=None):
     """Get all groups from Deadline"""
-    AVALON_SESSION = debug or avalon.Session["AVALON_DEADLINE"]
+    AVALON_SESSION = debug or api.Session["AVALON_DEADLINE"]
     argument = "{}/api/groups?NamesOnly=true".format(AVALON_SESSION)
     return query(argument)
 
